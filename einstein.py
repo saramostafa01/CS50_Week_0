069ff0d294e850163318dea23a184e6f94775bdf
saramostafa01 <saramostafafadlallah@gmail.com>
2024-09-28 17:21:15 +0300

    Add files via upload

---
+++ einstein.py
@@ -0,0 +1,8 @@
+# Prompt user to enter mass in kilograms
+mass = int(input("m = "))
+
+# Calculate the energy formulca E=mc^2
+energy = mass * pow(300000000,2)
+
+# Print the energy in joules
+print ("E = ",energy)
